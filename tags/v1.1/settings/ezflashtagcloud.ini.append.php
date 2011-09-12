7bd53085682a7e672b0b28571b690e47fa2ed148
Fiber_Optic <info@example.com>
2011-09-12 11:02:38 +0000

    security update
    git-svn-id: http://svn.projects.ez.no/ezflashtagcloud@8 3cff6e82-5cd6-4d8d-94aa-b8c9b8557a50

---
+++ tags/v1.1/settings/ezflashtagcloud.ini.append.php
@@ -0,0 +1,26 @@
+<?php /*
+
+[FlashSettings]
+Width=300
+Height=300
+BackgroundColor=ffffff
+
+#use transparent background
+Transparent=true
+
+#distr	String: true|false	If set to true, the tags are distributed evenly over the spheres surface.
+Distr=true
+
+#tcolor	Hex color value for	the default tag color
+Tcolor=052fff
+
+#tcolor2	Hex color value	Second tag color. If supplied, tags will get a color from a gradient between both colors based on their popularity.
+Tcolor2=6e7ecc
+
+#hicolor	Hex color value	Tag mouseover/hover color
+Hicolor=000000
+
+#tspeed	Number: percentage	Determines the speed of the sphere�s rotation. The default is 100, higher numbers increase the speed.
+Tspeed=100
+
+*/ ?>
